06c1e349b0bcdb37141d0d19e42cee92c74862d4
Sviatoslav <slavik2003>
2021-03-13 20:56:52 +0200

    some sketches

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include"Tkachuk.h"
+
+using namespace std;
+
+int main (int argc, char *argv[]) {
+
+    string args;
+
+    for (int i = 1; i < argc; i++) {
+        args += argv[i];
+    }
+
+    
+}
