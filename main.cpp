fcf1fbd06b90ffc09b0460033cbba5e5f1e81442
Sviatoslav <slavik2003>
2021-03-23 22:26:35 +0200

    Auto stash before merge of "master" and "origin/master"

--- main.cpp
+++ main.cpp
@@ -10,18 +10,10 @@ int main(int argc, char *argv[])
     {
         args += argv[i];
     }
-    string *expression = new string[argc];
+    string *expression = new string[args.size()];
+
     int expression_size;
     expressionSeparate(args, expression, &expression_size);
-    /*//////////////////////////////////////////// тимчасово
-    string *a = new string[argc - 1];
-
-    for (int i = 1; i < argc; i++)
-    {
-        a[i - 1] = argv[i];
-    }
-    ////////////////////////////////////////////*/
     cout << calculation(expression, expression_size) << endl;
-
     delete[] expression;
 }
