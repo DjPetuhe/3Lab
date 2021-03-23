8bddba1374901d6510daa97f3cb5bb16f6142af4
Valid <validhernufKPI@gmail.com>
2021-03-23 22:11:09 +0200

    Bugfixes

--- main.cpp
+++ main.cpp
@@ -3,26 +3,25 @@
 #include "Hernuf.h"
 using namespace std;
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     string args;
     for (int i = 1; i < argc; i++)
     {
         args += argv[i];
     }
     string *expression = new string[argc];
-    int expression_size = argc;
-    //expressionSeparate(args, expression, &expression_size);
-
-    //////////////////////////////////////////// тимчасово
+    int expression_size;
+    expressionSeparate(args, expression, &expression_size);
+    /*//////////////////////////////////////////// тимчасово
     string *a = new string[argc - 1];
 
     for (int i = 1; i < argc; i++)
     {
-        a[i-1] = argv[i];
+        a[i - 1] = argv[i];
     }
-    ////////////////////////////////////////////
-
-    cout << calculation(a, argc - 1) << endl;
+    ////////////////////////////////////////////*/
+    cout << calculation(expression, expression_size) << endl;
 
     delete[] expression;
 }
